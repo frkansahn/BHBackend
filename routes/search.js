8b2e6245ec65a2a74b5b4d8e31af70f44cbe7330
Furkan ŞAHİN <40474229+frkansahn@users.noreply.github.com>
2023-08-27 20:34:24 +0300

    .

--- routes/search.js
+++ routes/search.js
@@ -2,27 +2,15 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
-router.get('/*', function(req, res, next) {
-
+router.post('/*', function(req, res, next) {
+	var start = req.body.start || 0 , end = req.body.end || 20;
 	let search = req.params[0];
 
-	connection.query("CALL search_prosedur('"+search+"');" , function (results, error, fields) {
+	connection.query("CALL search_prosedur('"+search+"' , "+start+", "+end+");" , function (results, error, fields) {
 		if(error){
 			res.send({"status": 500, "error": error, "response": null}); 
 	  	} else {
 			results = results[0];
-			if(results) {
-				results = results.map(item => {
-					try {
-						if(item.image) {
-							item.image = JSON.parse(item.image);
-						}
-					}
-					catch(err){}
-
-					return item;
-				})
-			}
 	  		res.send({"status": 200, "success": "success","Fields":fields, "response": results});
   		}
   	});
